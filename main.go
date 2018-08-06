f6516e0c53c042cd80b34c1f4c7c7189aa50b796
dennougorilla <dennougorilla@gmail.com>
2018-08-06 18:44:09 +0900

    [add] main.go

---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+var accessToken = "EAAapjO8eyyoBANsd274NLUYfJixsO4GerD2moWkklHZCDMXLgWnvvrTaAOTE8iAHZAKXEGvZBDxZB3viYVjUbKDZCOx03M5OqTwbJ8OOkMgaJDgI2sMogHixalslOKKcdZBxuohZCxShdmJUZAsY0uI2pje9DLfTO4pyFMM1MJ6HdV6xZCwfaINPg"
+var verifyToken = "3460"
+
+const (
+	EndPoint = "https://graph.facebook.com/v2.6/me/messages"
+)
+
+type ReceivedMessage struct {
+	Object string  `json:"object"`
+	Entry  []Entry `json:"entry"`
+}
+
+type Entry struct {
+	ID        string      `json:"id"`
+	Time      int         `json:"time"`
+	Messaging []Messaging `json:"messaging"`
+}
+
+type Messaging struct {
+	Sender    Sender    `json:"sender"`
+	Recipient Recipient `json:"recipient"`
+	Timestamp int       `json:"timestamp"`
+	Message   Message   `json:"message"`
+}
+
+type Sender struct {
+	ID string `json:"id"`
+}
+
+type Recipient struct {
+	ID string `json:"id"`
+}
+
+type Message struct {
+	MID         string        `json:"mid"`
+	Seq         int           `json:"seq"`
+	Text        string        `json:"text"`
+	Attachments []Attachments `json:"attachments"`
+}
+
+type Attachments struct {
+	Type string `json:"type"`
+	//Payload string `json:"payload"`
+}
+type SendMessage struct {
+	Recipient Recipient `json:"recipient"`
+	Message   struct {
+		Text string `json:"text"`
+	} `json:"message"`
+}
+
+type TalkJson struct {
+	Status  int             `json:"status"`
+	Message string          `json:"message"`
+	Results []talkApiResult `json:"results"`
+}
+
+type talkApiResult struct {
+	Perplexity float64 `json:"perplexity"`
+	Reply      string  `json:"reply"`
+}
+
+func main() {
+	http.HandleFunc("/", TopPageHandler)
+	http.HandleFunc("/webhook", webhookHandler)
+	port := "5000"
+	address := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(address, nil)
+}
+
+func TopPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is go-bot application's top page.")
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		verifyTokenAction(w, r)
+	}
+	if r.Method == "POST" {
+		webhookPostAction(w, r)
+	}
+}
+
+func verifyTokenAction(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("hub.verify_token") == verifyToken {
+		log.Print("verify token success.")
+		fmt.Fprintf(w, r.URL.Query().Get("hub.challenge"))
+	} else {
+		log.Print("Error: verify token failed.")
+		fmt.Fprintf(w, "Error, wrong validation token")
+	}
+}
+
+func webhookPostAction(w http.ResponseWriter, r *http.Request) {
+	var receivedMessage ReceivedMessage
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Print(err)
+	}
+
+	if err = json.Unmarshal(body, &receivedMessage); err != nil {
+		log.Print(err)
+	}
+	messagingEvents := receivedMessage.Entry[0].Messaging
+	for i, event := range messagingEvents {
+		senderID := event.Sender.ID
+		log.Print(i)
+		if &event.Message != nil {
+			sendTextMessage(senderID, event.Message.Attachments[0].Type)
+		}
+
+	}
+	fmt.Fprintf(w, "Success")
+}
+
+func sendTextMessage(senderID string, text string) {
+	recipient := new(Recipient)
+	recipient.ID = senderID
+	m := new(SendMessage)
+	m.Recipient = *recipient
+	m.Message.Text = text
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		log.Print(err)
+	}
+
+	req, err := http.NewRequest("POST", EndPoint, bytes.NewBuffer(b))
+	if err != nil {
+		log.Print(err)
+	}
+
+	values := url.Values{}
+	values.Add("access_token", accessToken)
+	req.URL.RawQuery = values.Encode()
+	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
+	client := &http.Client{Timeout: time.Duration(30 * time.Second)}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Print(err)
+	}
+
+	defer res.Body.Close()
+	var result map[string]interface{}
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	if err := json.Unmarshal(body, &result); err != nil {
+		log.Print(err)
+	}
+	log.Print(result)
+}
